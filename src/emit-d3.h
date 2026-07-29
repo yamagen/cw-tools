4be1df8f6dad2816ed1a4ef73c85a019d131de02
Hilofumi Yamamoto <hilofumi.yamamoto@gmail.com>
2026-07-29 15:37:14 +0900

    Add external D3 data mode control

--- src/emit-d3.h
+++ src/emit-d3.h
@@ -1,9 +1,11 @@
 #ifndef EMIT_D3_H
 #define EMIT_D3_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include "emit-types.h"
 
+void emit_d3_set_data_only(bool enabled);
 void emit_d3_write(FILE *stream, const EdgeVec *edges,
                    const NodeVec *nodes, const Config *config);
 
